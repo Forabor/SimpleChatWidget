91cf8c5521d5f0c237cfaf4b3db15ff8d0d8eda6
Forabor <forabor@yahoo.com>
2016-03-18 01:14:43 -0400

    Initial commit

---
+++ simplechatwidget.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Simple Chat Widget
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Adds a widget for peer-to-peer chat.
+Version: 0.0.1
+Author: Corey Ferguson
+Author URI: http://www.gnu.org/licenses/gpl-2.0.html
+License: GPLv2 or later
+
+Simple Chat Widget is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Simple Chat Widget is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Simple Chat Widget. If not, see {License URI}.
+
+
+*/
+
+define( 'CAF_PLUGIN_VER', '0.0.1');
+define('CAF_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+?>
