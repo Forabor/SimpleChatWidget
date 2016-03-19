c40d98b773b44bb373fab9b69e76b43f722ac6cb
Forabor <forabor@yahoo.com>
2016-03-18 22:31:47 -0400

    Add Herko & Other Changes

--- simplechatwidget.php
+++ simplechatwidget.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 Plugin Name: Simple Chat Widget
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: Adds a widget for peer-to-peer chat.
@@ -7,7 +7,8 @@ Version: 0.0.1
 Author: Corey Ferguson
 Author URI: http://www.gnu.org/licenses/gpl-2.0.html
 License: GPLv2 or later
-
+*/
+/*
 Simple Chat Widget is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
@@ -24,74 +25,34 @@ along with Simple Chat Widget. If not, see {License URI}.
 
 */
 
-class wp_my_plugin extends WP_Widget {
-    // constructor
-    function wp_my_plugin() {
-        parent::WP_Widget(false, $name = __('Simple Chat Widget', 'wp_widget_plugin') );
-    }
-    // widget form creation
-    function form($instance) {
-// Check values
-if( $instance) {
-     $title = esc_attr($instance['title']);
-     $text = esc_attr($instance['text']);
-     $textarea = esc_textarea($instance['textarea']);
-} else {
-     $title = '';
-     $text = '';
-     $textarea = '';
-}
+define( 'CAF_PLUGIN_VER', '0.1.0');
+define('CAF_PLUGIN_URL', plugin_dir_url(__FILE__));
 
-    }
-    // widget update
-    function update($new_instance, $old_instance) {
-        $instance = $old_instance;
-        // Fields
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['text'] = strip_tags($new_instance['text']);
-        $instance['textarea'] = strip_tags($new_instance['textarea']);
-        return $instance;
-    }
-    // widget display
-    function widget($args, $instance) {
-        extract( $args);
-        //these are the widget options
-        $title = apply_filters('widget_title', $instance['title']);
-        $text = $instance['text'];
-        $text = $instance['textarea'];
-        echo $before_widget;
-        //Display the widget
-        echo '<div class = "widget-text wp_widget_plugin_box">';
-        //Check if the title is set
-        if( $title ){
-            echo $before_title . $title . $after_title;
-        }
-        //Check if the text is set
-        if( $text ){
-            echo '<p class="wp_widget_plugin_text">'.$text.'</p>';
-        }
-        //Check if the textarea is set
-        if( $textarea ){
-            echo '<p class="wp_widget_plugin_textarea">'.$textarea.'</p>';
-        }
-        echo "</div>";
-        echo $after_widget;
-    }
+add_action('wp_enqueue_scripts', 'simple_chat_plugin');
+
+function simple_chat_plugin()
+{
+    wp_enqueue_style('caf-css', CAF_PLUGIN_URL . 'assets/css/styles.css');
+    wp_enqueue_script('caf-js', CAF_PLUGIN_URL . 'assets/js/index.js', array('express, socket, http'), CAF_PLUGIN_VER, false);
 }
-// register widget
-add_action('widgets_init', create_function('', 'return register_widget("wp_my_plugin");'));
+//Html
+add_shortcode( 'index', 'html_shortcode');
+
+function html_shortcode(){
+    $content = <<<INDEX
+
+
+<div id="chatarea">
+    <ul id="messages"></ul>
+        <form action="">
+            <label for="m"></label><input id="m" autocomplete="off" /><button>Send</button>
+        </form>
+</div>
+
+
+INDEX;
+
+    return $content;
+}
 
 ?>
-<p>
-    <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Simple Chat Widget', 'wp_widget_plugin'); ?></label>
-    <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
-</p>
-<p>
-    <label for="<?php echo $this->get_field_id('text'); ?>"><?php _e('Text:', 'wp_widget_plugin'); ?></label>
-    <input class="widefat" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>" type="text" value="<?php echo $text; ?>" />
-</p>
-<p>
-    <label for="<?php echo $this->get_field_id('textarea'); ?>"><?php _e('Textarea:', 'wp_widget_plugin'); ?></label>
-    <textarea class="widefat" id="<?php echo $this->get_field_id('textarea'); ?>" name="<?php echo $this->get_field_name('textarea'); ?>"><?php echo $textarea; ?></textarea>
-</p>
-
